458e0e482c5949647cd353e89c3bfba10ee399b6
Navya33anji <90202407+Navya33anji@users.noreply.github.com>
2021-10-05 19:44:03 +0530

    Update Program.cs

--- Uc2/Program.cs
+++ Uc2/Program.cs
@@ -6,7 +6,17 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region UC2 peek and pop till empty
+            Console.WriteLine("-----UC2-----");
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Printstack();
+            stack.Peek();
+            stack.Pop();
+            stack.EmptyStack();
+            #endregion
+            Console.WriteLine("\n----------------------------------------------------");
         }
     }
 }
