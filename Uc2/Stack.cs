26a266dd16fa2d3c54e847a3437a6bbd21b8a8bc
Navya33anji <90202407+Navya33anji@users.noreply.github.com>
2021-10-05 19:45:02 +0530

    Update Stack.cs

--- Uc2/Stack.cs
+++ Uc2/Stack.cs
@@ -6,5 +6,23 @@ namespace StackAndQueue
 {
     class Stack
     {
+         private NodeStack top;
+
+        public void LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        // UC1 Push to Stack
+        internal void Push(int value)
+        {
+            NodeStack node = new NodeStack(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
     }
 }
