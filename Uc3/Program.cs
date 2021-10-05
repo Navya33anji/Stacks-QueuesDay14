51f578bc01ccba41b4d8cb90aaf6d1df6de0fdae
NAVYATHA MEKALA <navyatha487@gmail.com>
2021-10-05 20:13:54 +0530

    Ability to create Queue

---
+++ Uc3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            #region UC3 Enqueue- Creat queue
+            Console.WriteLine("-----UC3-----");
+            Queue_UC3_UC4 queue = new Queue_UC3_UC4();
+            queue.enqueue(56);
+            queue.enqueue(30);
+            queue.enqueue(70);
+            queue.PrintQueue();
+            #endregion
+            Console.WriteLine("\n----------------------------------------------------");
+        }
+    }
+}
