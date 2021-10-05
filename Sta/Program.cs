d7bdecdcf7cc1c3c1f49bdd8f3eff380da9ce3bc
NAVYATHA MEKALA <navyatha487@gmail.com>
2021-10-05 19:28:11 +0530

    Ability to Create a stack

---
+++ Sta/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region UC1 Creat Stack
+            Console.WriteLine("-----UC1-----");
+            Stack_UC1_UC2 stack = new Stack_UC1_UC2();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Printstack();
+            #endregion
+            Console.WriteLine("\n----------------------------------------------------");
+
+
+        }
+    }
+}
